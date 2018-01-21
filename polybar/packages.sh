87e68d1699a28e0f1de9ed95e8c3a4b7ce4bb1d2
Panagiotis Christou <cnotis@gmail.com>
2018-01-21 10:43:50 +0200

    Added AUR updates

--- polybar/packages.sh
+++ polybar/packages.sh
@@ -2,12 +2,12 @@
 
 PACKAGE_ARCH=$(checkupdates | wc -l)
 
-#PACKAGE_AUR=$(packer --quickcheck | wc -l)
+PACKAGE_AUR=$(cower -u | wc -l)
 
-#PACKAGE_TOTAL=
+PACKAGE_TOTAL=$(($PACKAGE_ARCH + $PACKAGE_AUR))
 
-echo "${PACKAGE_ARCH}"
+echo "${PACKAGE_TOTAL}"
 
-notify-send "There are ${PACKAGE_ARCH} official updates!"
+notify-send "There are ${PACKAGE_ARCH} official updates and ${PACKAGE_AUR} aur updates!"
 
 exit 0
