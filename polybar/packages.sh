6018c3eb8f57d79e3e98e36d2d087f3076fa53a6
Panagiotis Christou <cnotis@gmail.com>
2018-02-11 19:58:12 +0200

    Updated  packages script

--- polybar/packages.sh
+++ polybar/packages.sh
@@ -8,8 +8,16 @@ PACKAGE_TOTAL=$(($PACKAGE_ARCH + $PACKAGE_AUR))
 
 echo "${PACKAGE_TOTAL}"
 
-PACKAGE_LIST=$(yaourt -Qu)
-
 notify-send "There are ${PACKAGE_ARCH} official updates and ${PACKAGE_AUR} aur updates!"
-notify-send "${PACKAGE_LIST}"
+
+if [ ${PACKAGE_ARCH} -gt 0 ]; then
+	PACKAGE_LIST=$(checkupdates)
+	notify-send "ARCH packages:" \
+	"${PACKAGE_LIST}"
+fi
+if [ $PACKAGE_AUR -gt 0 ]; then
+	PACKAGE_AUR_LIST=$(cower -u)
+	notify-send "AUR packages:" \
+	"${PACKAGE_AUR_LIST}"
+fi
 exit 0
