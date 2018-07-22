931c1fa237617ef7e6093f123987dd4b653c526b
Unknown <c.notis@gmail.com>
2018-07-22 03:47:17 +0300

    updated layout script

--- scripts/layout.py
+++ scripts/layout.py
@@ -6,5 +6,18 @@ cmd = "herbstclient layout"
 wstr = ""
 
 output = str(os.popen(cmd).read())
-index = output.find(":")
-print(output[0:index])
+index = output.find("%")
+if output[4] == "g":
+    print(" ")
+elif output[4] == "v":    
+    if output[index] == "%":
+        print(" " + output[index-2:index+1])
+    else:
+        print(" ")
+elif output[4] == "h":
+    if output[index] == "%":
+        print(" "  + output[index-2:index+1])
+    else:
+       print(" " )
+elif output[4] == "m":
+    print(" ")
