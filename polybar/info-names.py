2d3a646d734c51512c40ac5f398ed7c5140120d0
Unknown <c.notis@gmail.com>
2018-11-04 02:03:06 +0200

    Updated polybar config - info-names.py

---
+++ polybar/info-names.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import feedparser
+from subprocess import call
+import re
+import textwrap
+
+d = feedparser.parse("http://www.eortologio.gr/rss/si_el.xml")
+
+for f in range(0, 1):
+    print(d.entries[f].title)
+
+   
+
+
+    
