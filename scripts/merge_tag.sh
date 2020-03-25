a9b71ab3c8a97ca5883458c5387df60cbfb790b7
Panagiotis Christou <c.notis@gmail.com>
2020-03-25 20:13:39 +0200

    [scripts] Add merge_tag.sh

---
+++ scripts/merge_tag.sh
@@ -0,0 +1,19 @@
+
+
+#!/usr/bin/env bash
+# merge_tag.sh
+
+# add keybind to autostart
+# hc keybind <keybind> spawn path/to/this/script/merge_tag.sh
+
+# i put my mod variables in here:
+#source ~/.config/herbstluftwm/vars.sh
+
+hc() {
+    herbstclient "$@"
+}
+
+tagname=$(rofi -dmenu -p "merge_tag" -lines 0 | xargs)
+
+
+hc merge_tag $tagname
