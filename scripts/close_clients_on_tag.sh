ac7f9629b5a20d603caa37f0552bb5e9179601e6
Panagiotis Christou <c.notis@gmail.com>
2020-08-19 18:29:25 +0300

    Add script which close all clients on tag

---
+++ scripts/close_clients_on_tag.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+closeallontag=(
+    foreach C clients.
+        substitute TAG tags.focus.name
+        sprintf WINIDATTR '%c.winid' C substitute WINID WINIDATTR
+        sprintf CTAGATTR '%c.tag' C  # the client's tag attribute
+        and . compare CTAGATTR = TAG
+            . close WINID
+)
+herbstclient "${closeallontag[@]}"
