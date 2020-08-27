3d22ea21a2171058001a038057812dab6dce853d
Panagiotis Christou <c.notis@gmail.com>
2020-08-28 00:22:07 +0300

    Update load_spawn_basic script

--- scripts/load_spawn_basic.sh
+++ scripts/load_spawn_basic.sh
@@ -16,19 +16,21 @@ spawn_with_rules alacritty -e tmux a -t home
 RULES=( tag=music hook=music index=1 once)
 spawn_with_rules alacritty -e cava
 
-herbstclient chain , emit_hook no_bsp , add "  " , load "  " "(split horizontal:0.26:1 (clients grid:0) (split horizontal:0.66:0 (split vertical:0.8:1 (clients grid:0) (clients grid:0)) (clients grid:0)))
-" , , emit_hook bsp
+herbstclient chain , emit_hook no_bsp , add "  " , use "  " , load "  " "(split horizontal:0.26:0 (split vertical:0.5:1 (clients grid:0) (clients grid:0)) (split horizontal:0.66:0 (split vertical:0.8:1 (clients grid:0) (clients grid:0)) (clients grid:0)))" , emit_hook bsp
 
-RULES=( tag="  " hook="  " index=0 )
+RULES=( tag="  " hook="  " index=00 )
 spawn_with_rules arandr
 
-RULES=( tag="  " hook="  " index=100 once)
+RULES=( instance=Gufw.py tag="  " hook="  " index=01 )
+spawn_with_rules  sudo gufw & disown 
+
+RULES=( tag="  " hook="  " index=100 )
 spawn_with_rules alacritty -e tmux a -t system
 
 RULES=( tag="  " hook="  " floating=off pseudotile=off index=110 )
 spawn_with_rules xfce4-power-manager-settings
 
-RULES=( tag="  " hook="  " index=101 once)
+RULES=( tag="  " hook="  " index=101 )
 spawn_with_rules alacritty
 
 herbstclient chain , emit_hook no_bsp , add "'  " , load "'  " "(split horizontal:0.7767:0 (clients max:0) (clients horizontal:0))" , emit_hook bsp
