9a1d4edb5532377f82ce27fc37d6d923be090742
Panagiotis Christou <c.notis@gmail.com>
2020-08-29 00:29:29 +0300

    Update load_spawn_basic script

--- scripts/load_spawn_basic.sh
+++ scripts/load_spawn_basic.sh
@@ -16,6 +16,8 @@ spawn_with_rules alacritty -e tmux a -t home
 RULES=( tag=music hook=music index=1 once)
 spawn_with_rules alacritty -e cava
 
+sleep 0.3
+
 herbstclient chain , emit_hook no_bsp , add "  " , use "  " , load "  " "(split horizontal:0.26:0 (split vertical:0.5:1 (clients grid:0) (clients grid:0)) (split horizontal:0.66:0 (split vertical:0.8:1 (clients grid:0) (clients grid:0)) (clients grid:0)))" , emit_hook bsp
 
 RULES=( tag="  " hook="  " index=00 )
@@ -33,6 +35,8 @@ spawn_with_rules xfce4-power-manager-settings
 RULES=( tag="  " hook="  " index=101 )
 spawn_with_rules alacritty
 
+sleep 0.3
+
 herbstclient chain , emit_hook no_bsp , add "'  " , load "'  " "(split horizontal:0.7767:0 (clients max:0) (clients horizontal:0))" , emit_hook bsp
 
 RULES=( class=discord tag="'  " hook="'  " index=0)
@@ -41,11 +45,19 @@ spawn_with_rules discord
 RULES=( tag="'  " hook="'  " index=1 )
 spawn_with_rules viber
 
+sleep 0.3
+
 RULES=( tag="  " hook="  " once)
 spawn_with_rules alacritty -e tmux a -t chat
 
+sleep 0.3
+
+herbstclient add "  "
+
 RULES=( tag="  " hook="  " once)
 spawn_with_rules alacritty -e tmux a -t scratchpad
+
+sleep 0.3
 
 herbstclient spawn pcmanfm
 
