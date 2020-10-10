295fd74e68d7344795e9ac9b0408cd827f1cb95d
Panagiotis Christou <c.notis@gmail.com>
2020-10-10 19:20:06 +0300

    Updated hide_window script.

--- scripts/hide_window.sh
+++ scripts/hide_window.sh
@@ -5,16 +5,18 @@ layout=$(herbstclient dump)
 
 case "$@" in
   hide) herbstclient chain . silent new_attr string clients.focus.my_layout "$layout" \
+                           . silent new_attr string clients.focus.my_prev_tag "$tag" \
 						   . add "'$tag" . move "'$tag" 
         ;;
-  unhide) herbstclient chain . use "'$tag"
-          layout=$(herbstclient attr clients.focus.my_layout)
-          herbstclient chain . remove_attr clients.focus.my_layout \
-          					 . use "$tag" \
-          					 . load "$tag" "$layout" \
-          					 . and , compare tags.by-name."'$tag".client_count = 0 , merge_tag "'$tag"
+  unhide) herbstclient chain : use "'$tag" \
+							 : substitute LAYOUT clients.focus.my_layout chain . remove_attr clients.focus.my_layout \
+                                . remove_attr clients.focus.my_prev_tag \
+          					 	. use "$tag" \
+          					 	. load "$tag" LAYOUT \
+          					 	. and , compare tags.by-name."'$tag".client_count = 0 , merge_tag "'$tag"
           ;;
-  use)    herbstclient use "'$tag"
+  use)    herbstclient or : and . substitute PREVIOUSTAG clients.focus.my_prev_tag chain , compare clients.focus.tag = "$tag" , use PREVIOUSTAG \
+                          : use "'$tag"  
           ;;
   all)    layout=$(herbstclient dump)
 		  herbstclient chain . add "*$tag*" . load "*$tag*" "$layout" . load "$tag" "$(<"$layouts/"default"")"
