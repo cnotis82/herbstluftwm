0a8b73d674a039b8d774e2187e5e566c7beefe0e
Panagiotis Christou <c.notis@gmail.com>
2020-12-14 01:40:31 +0200

    Updated minimize clients script

--- scripts/minimize_clients_on_tag.sh
+++ scripts/minimize_clients_on_tag.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
-
+#!/bin/sh
+tag=$(herbstclient attr tags.focus.name)
+layout=$(herbstclient dump)
 closeallontag=(
     foreach C clients.
         substitute TAG tags.focus.name
@@ -8,4 +9,11 @@ closeallontag=(
             . set_attr clients.focus.minimized true
             . remove
 )
-herbstclient "${closeallontag[@]}"
+case "$@" in
+  all)  layout=$(herbstclient dump)
+        herbstclient silent new_attr string tags.focus.my_minimize_layout "$layout" 
+        herbstclient "${closeallontag[@]}"
+        ;;
+  restore)  herbstclient substitute LAYOUT tags.focus.my_minimize_layout chain . remove_attr tags.focus.my_minimize_layout . load "$tag" LAYOUT
+          ;;
+esac
