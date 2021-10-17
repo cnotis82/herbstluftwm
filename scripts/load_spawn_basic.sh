78b0766fe252e51bb89b4c4d29d1e86d3cee1130
Panagiotis Christou <c.notis@gmail.com>
2021-10-17 18:41:27 +0300

    Updated theme and add telegram

--- scripts/load_spawn_basic.sh
+++ scripts/load_spawn_basic.sh
@@ -3,12 +3,12 @@
 spawn_with_rules() {(
     # this rule also requires, that the client
     # sets the _NET_WM_PID property
-    herbstclient rule pid=$BASHPID maxage=10 "${RULES[@]}"
+    hc_rs rule pid=$BASHPID maxage=120 "${RULES[@]}"
     exec "$@"
     ) &
 }
 
-herbstclient chain , emit_hook no_bsp , add music , load music "(split vertical:0.8:0 (clients grid:0) (clients grid:0))" , emit_hook bsp
+hc_rs chain , emit_hook no_bsp , add music , load music "(split vertical:0.8:0 (clients grid:0) (clients grid:0))" , emit_hook bsp
 
 RULES=( tag=music hook=music index=0 once)
 spawn_with_rules alacritty -e tmux a -t home
@@ -16,9 +16,9 @@ spawn_with_rules alacritty -e tmux a -t home
 RULES=( tag=music hook=music index=1 once)
 spawn_with_rules alacritty -e cava
 
-sleep 0.3
+sleep 0.5
 
-herbstclient chain , emit_hook no_bsp , add "  " , use "  " , load "  " "(split horizontal:0.26:0 (split vertical:0.5:1 (clients grid:0) (clients grid:0)) (split horizontal:0.66:0 (split vertical:0.8:1 (clients grid:0) (clients grid:0)) (clients grid:0)))" , emit_hook bsp
+hc_rs chain , emit_hook no_bsp , add "  " , use "  " , load "  " "(split horizontal:0.26:0 (split vertical:0.5:1 (clients grid:0) (clients grid:0)) (split horizontal:0.66:0 (split vertical:0.8:1 (clients grid:0) (clients grid:0)) (clients grid:0)))" , emit_hook bsp
 
 RULES=( tag="  " hook="  " index=00 )
 spawn_with_rules arandr
@@ -35,9 +35,9 @@ spawn_with_rules xfce4-power-manager-settings
 RULES=( tag="  " hook="  " index=101 )
 spawn_with_rules alacritty
 
-sleep 0.3
+sleep 0.5
 
-herbstclient chain , emit_hook no_bsp , add "'  " , load "'  " "(split horizontal:0.7767:0 (clients max:0) (clients horizontal:0))" , emit_hook bsp
+hc_rs chain , emit_hook no_bsp , add "'  " , load "'  " "(split horizontal:0.7767:0 (clients max:0) (clients horizontal:0))" , emit_hook bsp
 
 RULES=( class=discord tag="'  " hook="'  " index=0)
 spawn_with_rules discord
@@ -45,20 +45,24 @@ spawn_with_rules discord
 RULES=( tag="'  " hook="'  " index=1 )
 spawn_with_rules viber
 
-sleep 0.3
+RULES=( tag="'  " hook="'  " index=1 )
+spawn_with_rules telegram-desktop
+
+sleep 0.5
 
 RULES=( tag="  " hook="  " once)
 spawn_with_rules alacritty -e tmux a -t chat
 
-sleep 0.3
+sleep 0.5
 
-herbstclient add "  "
+hc_rs add "  "
 
 RULES=( tag="  " hook="  " once)
 spawn_with_rules alacritty -e tmux a -t scratchpad
 
-sleep 0.3
+sleep 0.5
 
-herbstclient spawn pcmanfm
+RULES=( tag="  " hook="  " once)
+spawn_with_rules alacritty -e tmux a -t file
 
-herbstclient spawn firefox
+hc_rs spawn firefox
